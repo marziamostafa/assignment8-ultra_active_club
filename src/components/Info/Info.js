63a4b29a2a9c9d7f5eb93d5dcbf2b5d239b91988
marziamostafa <marziamostafa@gmail.com>
2022-09-29 02:40:43 +0600

    fourth

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -4,7 +4,7 @@ import './info.css'
 const Info = () => {
     return (
         <div className='details'>
-            <div className='personal-info'>
+            <div className='personal-detail'>
                 <img src="https://i.pinimg.com/280x280_RS/2e/45/66/2e4566fd829bcf9eb11ccdb5f252b02f.jpg" alt="" />
                 <div>
                     <h4>Marzia Mostafa</h4>
@@ -18,7 +18,27 @@ const Info = () => {
                 <p><b>6.5</b><br />Height</p>
                 <p><b>25</b>years<br />Age</p>
             </div>
-            <div></div>
+            <div className='break'>
+                <h2>Add a break</h2>
+                <div className='buttons'>
+                    <button>10s</button>
+                    <button>20s</button>
+                    <button>30s</button>
+                    <button>40s</button>
+                </div>
+            </div>
+            <div className='exercise-details'>
+                <h2>Exercise Details</h2>
+                <div className='div1'>
+                    <h3>Exercise Time:</h3>
+                    <p>200 s</p>
+                </div>
+                <div className='div2'>
+                    <h3>Break Time:</h3>
+                    <p>20s</p>
+                </div>
+            </div>
+            <button className='complete'><p>Activity Completed</p></button>
         </div>
     );
 };
